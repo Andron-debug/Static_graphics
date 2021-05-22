19fa06bfeca6231b4e6d4fff12f1fd0c97ff9c55
Andron-debug <b.a.a.2002@yandex.ru>
2021-05-22 16:51:40 +0300

    Создан интерфейс. Отрисовка круга в круге

--- Static_graphics/Form1.Designer.cs
+++ Static_graphics/Form1.Designer.cs
@@ -29,13 +29,124 @@ namespace Static_graphics
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.X_trackBar = new System.Windows.Forms.TrackBar();
+            this.Y_trackBar = new System.Windows.Forms.TrackBar();
+            this.Whide_trackBar = new System.Windows.Forms.TrackBar();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.Color_button = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.X_trackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Y_trackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Whide_trackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(4, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(629, 440);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(662, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(662, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Y";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(666, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Тощина";
+            // 
+            // X_trackBar
+            // 
+            this.X_trackBar.Location = new System.Drawing.Point(662, 60);
+            this.X_trackBar.Name = "X_trackBar";
+            this.X_trackBar.Size = new System.Drawing.Size(130, 56);
+            this.X_trackBar.TabIndex = 4;
+            // 
+            // Y_trackBar
+            // 
+            this.Y_trackBar.Location = new System.Drawing.Point(662, 143);
+            this.Y_trackBar.Name = "Y_trackBar";
+            this.Y_trackBar.Size = new System.Drawing.Size(130, 56);
+            this.Y_trackBar.TabIndex = 5;
+            // 
+            // Whide_trackBar
+            // 
+            this.Whide_trackBar.Location = new System.Drawing.Point(662, 229);
+            this.Whide_trackBar.Minimum = 1;
+            this.Whide_trackBar.Name = "Whide_trackBar";
+            this.Whide_trackBar.Size = new System.Drawing.Size(130, 56);
+            this.Whide_trackBar.TabIndex = 6;
+            this.Whide_trackBar.Value = 1;
+            this.Whide_trackBar.Scroll += new System.EventHandler(this.Whide_trackBar_Scroll);
+            // 
+            // Color_button
+            // 
+            this.Color_button.Location = new System.Drawing.Point(666, 301);
+            this.Color_button.Name = "Color_button";
+            this.Color_button.Size = new System.Drawing.Size(126, 25);
+            this.Color_button.TabIndex = 7;
+            this.Color_button.Text = "Изменить цвет";
+            this.Color_button.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.Color_button);
+            this.Controls.Add(this.Whide_trackBar);
+            this.Controls.Add(this.Y_trackBar);
+            this.Controls.Add(this.X_trackBar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.X_trackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Y_trackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Whide_trackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar X_trackBar;
+        private System.Windows.Forms.TrackBar Y_trackBar;
+        private System.Windows.Forms.TrackBar Whide_trackBar;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button Color_button;
     }
 }
 
--- Static_graphics/Form1.cs
+++ Static_graphics/Form1.cs
@@ -10,12 +10,32 @@ using System.Windows.Forms;
 
 namespace Static_graphics
 {
+    
     public partial class Form1 : Form
     {
+        Graphics g;
+        Pen p = new Pen(System.Drawing.Color.Black);
+        int cout_x;
+        int cout_y;
         public Form1()
         {
             InitializeComponent();
+            p.Width = Whide_trackBar.Value;
+            g = pictureBox1.CreateGraphics();
+           
+        }
+        private void Draw()
+        {
+            g.Clear(System.Drawing.Color.White);
+            g.DrawEllipse(p, 20, 20, 100, 100);
+            g.DrawEllipse(p, 45, 45, 50, 50);
+            
         }
 
+        private void Whide_trackBar_Scroll(object sender, EventArgs e)
+        {
+            p.Width = Whide_trackBar.Value;
+            Draw();
+        }
     }
 }
